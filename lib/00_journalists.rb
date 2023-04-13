f7a7d20144f831dba25da061c2031a8bc5415409
Camille Lenormand <camille.lenormand02@gmail.com>
2023-04-13 15:15:36 +0200

    Add project to Github

---
+++ lib/00_journalists.rb
@@ -0,0 +1,152 @@
+# Global variables
+
+$data = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# Remove at from hash with a map function to replace @ symbol with empty space
+ at
+$data_without_at = $data.map{
+  | string | string.gsub("@", "")
+}
+
+#---------------------------------------------------------------------------------------#
+
+# Combien y a-t-il de handle dans cette array ?
+def sum
+  puts "On a #{$data.size} handles dans cette array."
+end
+
+#---------------------------------------------------------------------------------------#
+
+# Quel est le handle le plus court de cette liste ?
+
+def shortest
+# Use min_by function to retrieve element with the shortest length
+  puts "Le handle le plus court est #{$data.min_by(&:length)}."
+end
+
+#---------------------------------------------------------------------------------------#
+
+# Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+
+def size
+# Select hash with length == 5
+  result = $data_without_at.select {
+    | key | key.length == 5
+  }.count
+puts "Il y a #{result} handles qui contiennent 5 caractères."
+end
+
+#---------------------------------------------------------------------------------------#
+
+# Combien commencent par une majuscule (première lettre juste après le @) ?
+
+def find_uppercase
+# Use grep function to isolate elements with uppercase then count them
+  uppercase_items = $data_without_at.grep(/^[A-Z]/).count
+  puts "Il y a #{uppercase_items} handles qui commencent par une majuscule."
+end
+
+#---------------------------------------------------------------------------------------#
+
+# Trie la liste des handles par ordre alphabétique.
+
+# Use sort to display elements alphabetically
+def sort
+  puts "Voici la liste des handles triés par ordre alphabétique: #{$data.sort}."
+end
+
+#---------------------------------------------------------------------------------------#
+
+# Trie la liste des handles par ordre croissant
+
+# Sort elements in the array by length comparing elements one by one
+def sort_asc
+  puts "Tri des handles par ordre croissant: "
+  result = $data.sort {
+    | left, right | left.length <=> right.length}
+    puts result
+end
+
+#---------------------------------------------------------------------------------------#
+
+# Quelle est la position dans l'array de la personne @epenser ?
+
+# Retrieve the index of the element 
+def find_index
+  puts "la position dans l'array de la personne @epenser est #{$data.index("@epenser")}."
+end
+
+#---------------------------------------------------------------------------------------#
+
+# Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+
+def group_by_size
+  puts "Répartition des handles par taille: " 
+  # Group elements by length then return a new hash with the results of counting each element
+  puts $data_without_at.group_by(&:length).transform_values(&:count)
+end
+
+#---------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------#
+
+# Initialize the menu options
+$menu_options = [
+  {
+    name: "Combien y a-t-il de handle dans cette array ?",
+    method: method(:sum)
+  },
+  {
+    name: "Quel est le handle le plus court de cette liste ?",
+    method: method(:shortest)
+  },
+  {
+    name: "Combien y-a-t'il de handle contenant 5 caractères ?",
+    method: method(:size)
+  },
+  {
+    name: "Combien commencent par une majuscule (première lettre juste après le @) ?",
+    method: method(:find_uppercase)
+  },
+  {
+    name: "Trie la liste des handles par ordre alphabétique.",
+    method: method(:sort)
+  },
+  {
+    name: "Trie la liste des handles par ordre croissant",
+    method: method(:sort_asc)
+  },
+  {
+    name: "Quelle est la position dans l'array de la personne @epenser ?",
+    method: method(:find_index)
+  },
+  {
+    name: "Sors-moi une répartition des handle par taille.",
+    method: method(:group_by_size)
+  }
+]
+
+# Define the method to display the menu
+def display_menu
+  system("clear")
+  puts "Sélectionnez une des options ci-dessous en tapant le numéro correspondant:\n"
+  $menu_options.each_with_index do |option, index|
+    puts "#{index + 1}. #{option[:name]}"
+  end
+  print "\nEnter your choice: "
+  choice = gets.chomp.to_i
+  if choice > 0 && choice <= $menu_options.length
+    $menu_options[choice - 1][:method].call
+    puts "\nPress enter to continue..."
+    gets
+    display_menu
+  else
+    puts "Invalid choice. Please try again."
+    gets
+    display_menu
+  end
+end
+
+# Call the method to display the menu
+display_menu
+
+
